c959c11ebd40b8a5515d5c867dac4f23be824545
strux12 <savithreddyr@gmail.com>
2025-05-22 09:30:26 +0530

    Add files via upload

---
+++ 2nd assignment.py
@@ -0,0 +1,7 @@
+#Task 1 :
+n= int(input("Enter a number :"))
+if n / 2 is int :
+    print("Even number")
+else :
+    print("Odd number")
+    
---
+++ Assignment 2 (Task 2).py
@@ -0,0 +1,7 @@
+#Task 2
+n=int(50)
+total=n+0
+for i in range(1,50):
+    total=total + i
+print("The sum of numbers from 1 to 50 is :", total)
+print("The sum of numbers from 1 to 50 is :", total)
